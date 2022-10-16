2b7f7690ac6da6931a37bdba3ef0c5c66644d009
percy cordova <cordova_inf@hotmail.com>
2022-10-16 00:50:10 -0500

    prueba"

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true,
   images: {
     domains: ['cloudfront-us-east-1.images.arcpublishing.com', 'cdn.larepublica.pe'],
     layoutRaw: true
