c2a5135807140c2e463c3a83689b26df7b90ce3c
percy cordova <cordova_inf@hotmail.com>
2022-10-17 03:22:15 -0500

    pruebas

--- components/graphics/BarChart.jsx
+++ components/graphics/BarChart.jsx
@@ -1,28 +1,28 @@
-import React, { useEffect } from 'react'
-import { Bar } from 'react-chartjs-2'
-import ChartDataLabels from 'chartjs-plugin-datalabels'
-import Chart from 'chart.js/auto'
+import React, { useEffect } from "react";
+import { Bar } from "react-chartjs-2";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import Chart from "chart.js/auto";
 const BarChart = ({ data }) => {
-  const options = {
+/*  const options = {
     responsive: true,
     plugins: {
       datalabels: {
         display: true,
-        color: 'gray',
+        color: "gray",
         formatter: Math.round,
-        anchor: 'end',
+        anchor: "end",
         offset: -20,
-        align: 'start'
-      }
+        align: "start",
+      },
     },
     legend: {
-      display: false
-    }
-  }
+      display: false,
+    },
+  };
   useEffect(() => {
-    Chart.register(ChartDataLabels)
-  }, [])
-  return <Bar data={data} options={options} />
-}
+    Chart.register(ChartDataLabels);
+  }, []);*/
+  return <Bar data={data}  />;
+};
 
-export default BarChart
+export default BarChart;
